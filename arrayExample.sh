ce06fdf6def05c383db3e0a262672a3ad350ee51
Darpan Madlani <Darpan@proclink.com>
2022-02-26 09:39:08 +0530

    Array example is done

---
+++ arrayExample.sh
@@ -0,0 +1,11 @@
+cnt=0;
+fruits[((cnt++))]="Mango";
+fruits[((cnt++))]="Oranges";
+fruits[((cnt++))]="Grapes";
+fruits[((cnt++))]="Banana";
+
+echo ${fruits[@]};
+
+echo ${fruits[2]};
+
+echo "Size of an array : " ${#fruits[@]};
